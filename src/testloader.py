43ad30f8c1ee26c7056bc969c05f32fbc8d59cf0
susTuna <hidasetsuna@gmail.com>
2024-05-15 17:49:25 +0700

    fixed conflicts in merged csv added constants to F08, fixed testloader.py

--- src/testloader.py
+++ src/testloader.py
@@ -1,38 +1,38 @@
-import glbfunc
+import operateCSV
 
 #constants
-uid=0
-m_id=1
-mlv=2
+u_id=0
+u_m_id=1
+u_m_lv=2
 
-mo_id=0
-type=1
-atk=2
-gua=3
-hp=4
-lv=1
+m_id=0
+m_type=1
+m_atk=2
+m_gua=3
+m_hp=4
+m_lv=1
 
-monster=glbfunc.csv_reader('monster.csv')
-monster_list=glbfunc.csv_parser(monster,';',11,5)
+monster=operateCSV.baca_csv(r'data\monster.csv')
 
-monstinv=glbfunc.csv_reader('monster_inventory.csv')
-monstinv_list=glbfunc.csv_parser(monstinv,';',9,3)
+monstinv=operateCSV.baca_csv(r'data\monster_inventory.csv')
 
 user_monster=[['monster_id','monster_level']]
 monster_invent=[['type','atk','def','hp','lv']]
 
-def filter_monster(monsterinv_list,user):
+#filter monster berdasarkan data user
+def filter_monster(monsterinv_list : list,user : int) -> list:
     for data in monsterinv_list:
-        if data[uid]==user:
-            user_monster.append([data[m_id],data[mlv]])
+        if data[u_id]==user:
+            user_monster.append([data[u_m_id],data[u_m_lv]])
     return user_monster
 
-def monster_inventory(monster_list,user_monster):
+def monster_inventory(monster_list : list,user_monster : list) -> list:
     for monster in monster_list: 
        for usermon in user_monster:
-            if monster[mo_id]==usermon[mo_id]:
-                monster_invent.append([monster[type],monster[atk],monster[gua],monster[hp],usermon[lv]])
+            if monster[m_id]==usermon[m_id]:
+                monster_invent.append([monster[m_type],monster[m_atk],monster[m_gua],monster[m_hp],usermon[m_lv]])
     return monster_invent
 
+
 user=(input('uid: '))
-print(monster_inventory(monster_list,filter_monster(monstinv_list,user)))
+print(monster_inventory(monster,filter_monster(monstinv,user)))
